9ab4b361261d6f3f2d9560b5311d103fc08a52e0
treejamie <me@treejamie.com>
2025-03-07 12:07:07 +0000

    banking progress and doing the stuff I am supposed to be doing

---
+++ MITx6.00.1x/unit5-object-orientated-programming/fex3.py
@@ -0,0 +1,24 @@
+class Coordinate(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+    
+
+    def __eq__(self, cmp):
+        return self.x == cmp.x and self.y == cmp.y 
+
+    def __repr__(self):
+        return 'Coordinate({0},{1})'.format(self.x, self.y)
+        
