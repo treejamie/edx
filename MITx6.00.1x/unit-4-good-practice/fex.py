85b186e20b14bbc44e67e7d535fc09586396410d
Jamie <1866+treejamie@users.noreply.github.com>
2025-03-12 20:42:40 +0000

    Revert "Unit5"

--- MITx6.00.1x/unit-4-good-practice/fex.py
+++ MITx6.00.1x/unit-4-good-practice/fex.py
@@ -10,9 +10,11 @@ Also, you know, time. The chances of my death increase each day.
 
 def normalize(numbers):
     max_number = max(numbers)
-    assert(max_number != 0), "Cannot divide by 0"
     for i in range(len(numbers)):
-        numbers[i]  /= float(max_number)
-        assert(0.0 <= numbers[i] <= 1.0), "output not between 0 and 1"
-    return numbers        
-        
+        numbers[i] /= float(max_number)
+    return numbers  
+
+try:
+      normalize([0, 0, 0])
+except ZeroDivisionError:
+      print('Invalid maximum element')
